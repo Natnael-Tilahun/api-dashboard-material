35179c274250c6398381510e443b6d18ae405cba
Natnael-Tilahun <natnaeltilahun97gmail.com>
2021-12-08 14:54:14 +0300

    add forms for new user registration

--- Layouts/newuserContainer.js
+++ Layouts/newuserContainer.js
@@ -1,9 +1,72 @@
+import { Email, Password, Phone } from '@mui/icons-material';
+import {
+  Grid,
+  List,
+  ListItem,
+  Paper,
+  TextField,
+  Typography,
+} from '@mui/material';
 import React from 'react';
 
 export default function newuserContainer() {
   return (
-    <div sx={{ padding: 0 }}>
-      <h1>new user container</h1>
-    </div>
+    <Grid container style={{ display: 'flex', justifyContent: 'center' }}>
+      <Paper sx={{ padding: 10 }}>
+        <form style={{ minWidth: 800 }}>
+          <Typography component="h5" variant="h5">
+            User Registration
+          </Typography>
+          <List>
+            <ListItem>
+              <TextField
+                variant="outlined"
+                fullWidth
+                id="fullname"
+                label="Full Name"
+                inputProps={{ type: Text }}
+                
+              ></TextField>
+            </ListItem>
+            <ListItem>
+              <TextField
+                variant="outlined"
+                fullWidth
+                id="phone"
+                label="Phone Number"
+                inputProps={{ type: Phone }}
+              ></TextField>
+            </ListItem>
+            <ListItem>
+              <TextField
+                variant="outlined"
+                fullWidth
+                id="email"
+                label="Email"
+                inputProps={{ type: Email }}
+              ></TextField>
+            </ListItem>
+            <ListItem>
+              <TextField
+                variant="outlined"
+                fullWidth
+                id="password"
+                label="Password"
+                inputProps={{ type: Password }}
+              ></TextField>
+            </ListItem>
+            <ListItem>
+              <TextField
+                variant="outlined"
+                fullWidth
+                id="confirmPasword"
+                label="Confirm Password"
+                inputProps={{ type: Password }}
+              ></TextField>
+            </ListItem>
+          </List>
+        </form>
+      </Paper>
+    </Grid>
   );
 }
